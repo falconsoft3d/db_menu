64bbfc32f5d53f7bb1559a31a9c6e5fc67878efb
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2018-07-16 12:59:59 -0400

    Update __manifest__.py

--- db_menu/__manifest__.py
+++ db_menu/__manifest__.py
@@ -23,7 +23,7 @@
 {
     'name': 'DB Menu MFH',
     'version': '10.0.0.1.0',
-    'author': "Falcón Solutions",
+    'author': "Falcon Solutions SpA",
     'maintainer': 'Falcon Solutions',
     'website': 'http://www.falconsolutions.cl',
     'license': 'AGPL-3',
